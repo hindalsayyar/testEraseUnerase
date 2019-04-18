db4717449d60b6a21851743801fd992370552617
imedev4 <imedev4@imedev4s-macbook-pro.local>
2019-04-18 10:51:56 +0300

    Initial Commit

---
+++ testEraseUnerase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testEraseUnerase
+//
+//  Created by imedev4 on 18/04/2019.
+//  Copyright © 2019 5W2H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
